b763f51c303d40b34c93df05d75b12dca405d1d2
sunilgori2000@gmail.com <sunilgori22@gmail.com>
2021-03-16 15:40:31 +0530

    1. bug fixed

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadService } from '../../services/upload.service';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import { AngularFireDatabase } from '@angular/fire/database';
+import {UserService} from '../../services/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  fileName;
+  fileName1;
+  path = 'images/users/';
+  imgId: string;
+  private vUid;
+
+  constructor(private upSer: UploadService , private db: AngularFireDatabase, private userSer: UserService) {
+
+    this.vUid = sessionStorage.getItem('uid');
+
+    this.db.list('/users', ref => ref.orderByChild('key').equalTo(this.vUid)).valueChanges()
+      .subscribe( data => this.getImg(data));
+  }
+
+  ngOnInit(): void {
+  }
+
+  async getImg(data){
+    let temp = await data[0].image;
+    let firStore = firebase.storage();
+    try{
+      let imgLink = await  firStore.ref(`/images/users/${ temp }`).getDownloadURL();
+      this.fileName = imgLink;
+    }
+    catch (e) {
+      if (e.code === 'storage/object-not-found'){
+        console.log('Image not found!!');
+      }
+    }
+
+  }
+
+  uploadImg(img: HTMLInputElement){
+    this.upSer.upload(img, this.fileName1);
+    this.userSer.updateImg(this.vUid, img.files.item(0).name);
+  }
+
+
+  viewFile(imgUpload: HTMLInputElement){
+    console.log('File :: ' + imgUpload.files.item(0).name);
+    const reader = new FileReader();
+    reader.readAsDataURL(imgUpload.files[0]);
+    reader.onload = (e) => {
+      this.fileName = reader.result;
+    };
+  }
+}
---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  private uploadTask: firebase.storage.UploadTask;
+  storageRef = firebase.storage().ref();
+  constructor() { }
+
+  upload(imgUpload: HTMLInputElement, strLink: string){
+    this.uploadTask = this.storageRef.child(`images/users/${imgUpload.files[0].name}`).put(imgUpload.files[0]);
+    this.uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED , ( snapshot) => {
+      console.log('uploading :: ' + snapshot.bytesTransferred / snapshot.totalBytes * 100);
+    }, ( err ) => {
+      console.log( 'error occured :: ' + err.message[0] );
+    }, ( ) => {
+      this.uploadTask.snapshot.ref.getDownloadURL().then(( downloadUrl) => {
+        console.log('Download the image => ' + downloadUrl);
+        strLink = downloadUrl;
+      });
+      console.log('File uploaded successfully!!!');
+    } );
+  }
+
+  getUrl(path: string, imgId: string, imgCtr: HTMLImageElement){
+    console.log('path :: ' + path + ' imgId :: ' + imgId);
+    const downloadImg = this.storageRef.child(`${path}${imgId}`).getDownloadURL().then( (data) => {
+      imgCtr.src = data[0].image;
+    }, ( error) => {
+      console.log('Error occured :: ' + error.message[0]);
+    });
+  }
+}
